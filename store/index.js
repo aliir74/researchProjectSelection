bdbd09c66a15aed0f43d1470e064ce0f213d1a49
aliir74 <aliirani74@gmail.com>
2017-09-25 21:57:27 +0330

    vue-cookie package doesnt work :/

--- store/index.js
+++ store/index.js
@@ -1,5 +1,9 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
-// import createPersistedState from 'vuex-persistedstate'
+import createPersistedState from 'vuex-persistedstate'
+// import * as Cookie from 'js-cookie'
+var VueCookie = require('vue-cookie')
+Vue.use(VueCookie)
 
 const store = () => {
   return new Vuex.Store({
@@ -20,7 +24,22 @@ const store = () => {
         state.name = name
         state.grade = grade
       }
-    }
+    },
+    plugins: [
+      createPersistedState({
+        paths: ['user'],
+        storage: {
+          /*
+          getItem: (key) => Cookie.getJSON(key),
+          setItem: (key, value) => Cookie.set(key, value, { expires: 3, secure: true }),
+          removeItem: (key) => Cookie.remove(key)
+          */
+          getItem: (key) => Vue.cookie.get(key),
+          setItem: (key, value) => Vue.cookie.set(key, value, { expires: 3 }),
+          removeItem: (key) => Vue.cookie.delete(key)
+        }
+      })
+    ]
   })
 }
 
